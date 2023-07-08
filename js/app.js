fcac95f21729ac3c82f01a896de8a371777fa9e9
Diego Rocha <drochaesc@gmail.com>
2023-07-07 18:24:10 -0600

    Validando formulario de agregar gasto
    Mostrando alertas en el HTML

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const gastoListado = document.querySelector('#gastos ul');
 eventListeners();
 function eventListeners() {
     document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
-    // formulario.addEventListener('submit',);
+    formulario.addEventListener('submit', agregarGasto);
 }
 
 
@@ -20,7 +20,33 @@ class Presupuesto {
 }
 
 class UI {
+    insertarPresupuesto(cantidad){
+        const {presupuesto, restante} = cantidad;
+        document.querySelector('#total').textContent = presupuesto;
+        document.querySelector('#restante').textContent = restante;
+    }
+    mostrarAlerta(mensaje, tipo){
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('text-center', 'alert');
+        
+        if(tipo === 'error'){
+            divMensaje.classList.add('alert-danger');
+        }
+        else {
+            divMensaje.classList.add('alert-success');
+        }
 
+        // Agregar contenido
+        divMensaje.textContent = mensaje;
+
+        // Insertar al HTML
+        document.querySelector('.primario').insertBefore(divMensaje, formulario);
+        
+        // Eliminar Alerta
+        setTimeout(() => {
+            divMensaje.remove();
+        }, 3000);
+    }
 }
 
 // Instancias
@@ -31,7 +57,6 @@ let objPresupuesto;
 
 function preguntarPresupuesto() {
     const presupuestoUsuario = prompt('Ingresa tu presupuesto disponible:');
-    console.log( Number(presupuestoUsuario) );
 
     if (presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0) {
         window.location.reload();
@@ -39,4 +64,28 @@ function preguntarPresupuesto() {
 
     objPresupuesto = new Presupuesto(presupuestoUsuario);
     console.log(objPresupuesto);
+
+    objUI.insertarPresupuesto(objPresupuesto);
+}
+
+function agregarGasto(e) {
+    e.preventDefault();
+
+    // Leer datos de formulario
+    const nombre = document.querySelector('#gasto').value.trim();
+    const cantidad = document.querySelector('#cantidad').value.trim();
+
+    // Validar
+    if( nombre === '' || cantidad === ''){
+        objUI.mostrarAlerta('Ambos campos son obligatorios', 'error');
+        return;
+    }
+    else if( cantidad <= 0 || isNaN(cantidad)){
+        objUI.mostrarAlerta('El presupuesto no es válido', 'error');
+        return;
+    }
+    else if( !isNaN(nombre)){
+        objUI.mostrarAlerta('El nombre debe ser de formato texto', 'error');
+        return;
+    }
 }
